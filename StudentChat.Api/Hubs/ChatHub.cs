3fd9066b9c32391e3af0df399244bdd2aca2f108
martinengstromm <martin.engstromm@gmail.com>
2026-09-01 13:02:23 +0200

    SignalR hubb

---
+++ StudentChat.Api/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace StudentChat.Api.Hubs
+{
+	// Hanterar realtidskommunikationen i chatten
+	public class ChatHub : Hub
+	{
+		// Skickar ett meddelande till alla anslutna användare
+		public async Task SendMessage(string user, string message)
+		{
+			await Clients.All.SendAsync("ReceiveMessage", user, message);
+		}
+	}
+}
--- StudentChat.Api/Program.cs
+++ StudentChat.Api/Program.cs
@@ -1,10 +1,16 @@
+using StudentChat.Api.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Ativera SignalR
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -19,5 +25,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Kopplat ChatHub till /chatHub
+app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
